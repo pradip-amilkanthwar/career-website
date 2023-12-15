f0ab42eaf7fb8d644db375776d1d56792650a078
pradip-amilkanthwar <71257647+pradip-amilkanthwar@users.noreply.github.com>
2023-12-15 13:48:06 +0000

    Added dynamic jobs list and API to fetch jobs as JSON object.

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': '1',
+    'title': 'Lead Engineer',
+    'location': 'India Pune'
+}, {
+    'id': '2',
+    'title': 'Data Analyst',
+    'location': 'India Pune',
+    'salary': '2000000'
+}, {
+    'id': '3',
+    'title': 'Data Scientist',
+    'location': 'India Pune/Mumbai',
+    'salary': '2000000'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Avni Infotech')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
